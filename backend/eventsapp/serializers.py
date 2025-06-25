a4d286becba3f5208d5b0d5f74428a8bddbfaf6a
bettynjeri001 <bettywachira14@gmail.com>
2025-06-25 16:00:00 +0000

    Commit changes

---
+++ backend/eventsapp/serializers.py
@@ -0,0 +1,59 @@
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+from .models import CustomUser, UserProfile, Eventsapp, Registration
+
+# User Serializer (basic details)
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'role']
+
+
+# Register Serializer
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password', 'role']
+
+    def create(self, validated_data):
+        return CustomUser.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            role=validated_data['role']
+        )
+
+
+# Login Serializer
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Invalid credentials")
+
+
+# UserProfile Serializer
+class UserProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = UserProfile
+        fields = ['user', 'bio', 'profile_picture', 'location']
+
+
+# Eventsapp Serializer
+class EventsappSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Eventsapp
+        fields = ['id', 'user', 'title', 'description', 'date', 'time', 'location', 'ticket', 'image']
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Registration
+        fields = ['id', 'event', 'name', 'email', 'registered_at']
